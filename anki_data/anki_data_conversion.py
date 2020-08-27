8618e927e1b8911471df5795562322ed4e6fb19b
Malo Mn <malo.maisonneuve@gmail.com>
2020-08-27 20:17:05 +0200

    Modified data structure
    After some manipulations in Godot, I figured this architecture would
    best suit my needs.

--- anki_data/anki_data_conversion.py
+++ anki_data/anki_data_conversion.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 import logging
 import pandas as pd
+import re
 
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig(filename="anki_data_conversion.log",
@@ -12,9 +13,23 @@ logger = logging.getLogger()
 
 def extract_url(name):
     if type(name) is str:
-        return name[13:len(name)-4].replace('.svg', '.png')
+        output = name[13:len(name)-4].replace('.svg', '.png')
+        output = re.findall(r'-(.*)\.', output)[0]
+        output = output.replace('-nobox', '')
     else:
-        return name
+        output = name
+    return output
+
+
+def replace_value_by_x(info):
+    if type(info) is str:
+        return 'x'
+    else:
+        return info
+
+
+def extract_tags(tag):
+    return '.'.join(re.findall(r'UG::(\w+)', tag))
 
 
 class Data:
@@ -24,21 +39,27 @@ class Data:
                        'Flag similarity', 'Map', 'tags']
         self.data = pd.read_csv(path)
         self.data = self.data.filter(self.header)
-        # Remove html from urls
+        # Remove html from urls and get the shortname of the country
         self.data['Map'] = self.data['Map'].apply(extract_url)
-        self.data['Flag'] = self.data['Flag'].apply(extract_url)
+        self.data['short'] = self.data['Map']
+        # Copy Capital column to create the Pinpoint column
+        self.data['Pinpoint'] = self.data['Capital']
+        # Replacing the values by x
+        self.data['Flag'] = self.data['Flag'].apply(replace_value_by_x)
+        self.data['Map'] = self.data['Map'].apply(replace_value_by_x)
+        self.data['Pinpoint'] = self.data['Pinpoint'].apply(replace_value_by_x)
+        self.data.rename(columns={'Map': 'Polygon'}, inplace=True)
+        # Extract the tags
+        self.data['tags'] = self.data['tags'].apply(extract_tags)
         # Quick sort by Country
         self.data = self.data.sort_values(['Country'])
         self.data = self.data.reset_index(drop=True)
-        self.manual_replacements()
 
-    def manual_replacements(self):
-        self.data.loc[27, 'Flag'] = 'flag-bali.png'
-        self.data.loc[42, 'Flag'] = 'flag-bolivia.png'
-        self.data.loc[92, 'Flag'] = 'flag-el_salvador.png'
-        self.data.loc[120, 'Flag'] = 'flag-guam.png'
-        self.data.loc[201, 'Flag'] = 'flag-nicaragua.png'
-        self.data.loc[221, 'Flag'] = 'flag-paraguay.png'
+        # Set the columns in a specific order
+        self.data = self.data[['Country', 'Country info', 'Capital', 'Capital info', 'Capital hint', 'Flag',
+                               'Flag similarity', 'Polygon', 'Pinpoint', 'short', 'tags']]
+
+        print(self.data.iloc[0])
 
     def replace(self, value, replacement):
         temporary = self.data.copy(deep=True)
@@ -75,7 +96,7 @@ if __name__ == "__main__":
     tab = Data('data.csv')
     tab.replace('Australia (Oceania)', 'Oceania')
     # TODO check that the following line is correctly working
-    tab.add_line(['Saint Pierre and Miquelon', 'Saint Pierre'] + ['' for i in range(7)])
+    # tab.add_line(['Saint Pierre and Miquelon', 'Saint Pierre'] + ['' for i in range(7)])
     # tab.remove('United Kingdom')
     tab.save('cleaned_data.csv')
-    print(tab.data.Country)
+    print(tab.data.iloc[27])
