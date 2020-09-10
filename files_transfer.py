77ba45d8b22b5d17e3e2ff96e217103242bf0189
Malo Mn <malo.maisonneuve@gmail.com>
2020-09-10 22:17:31 +0200

    Script copying files from one repo to the other

---
+++ files_transfer.py
@@ -0,0 +1,39 @@
+import pathlib
+import pandas as pd
+from shutil import copyfile
+
+current_folder = str(pathlib.Path().absolute()) + '\\'
+destination_folder = current_folder.replace("map_data_generator", "Map") + 'data\\'
+print("Copying data: {} --> {}\n".format(current_folder, destination_folder))
+
+# Gathering data
+data = pd.read_csv("anki_data/cleaned_data.csv")
+
+# Filtering the data to keep wanted lines and header
+transfer_choice = data.apply(lambda x: 'Sovereign_State' in x.tags, axis=1)
+data = data[transfer_choice]
+data = data.drop(data.columns[0], axis=1)
+data = data.reset_index()
+del data['index']
+
+# Save this array in the dest_folder
+data.to_csv(destination_folder + 'data.csv', sep=',')
+print(data.head())
+
+# Getting the shorts of chosen areas
+shorts = list(data.short)
+
+# Copy the polygons, pinpoints, flags
+for f in ['pinpoints', 'polygons_collision', 'polygons_display', 'flags']:
+    for s in shorts:
+        name = f + '\\' + s
+        name = name + '.json' if f != 'flags' else name + '.png'
+
+        try:
+            copyfile(current_folder + name, destination_folder + name)
+        except FileNotFoundError:
+            print("Could not find " + current_folder + name)
+
+    print("Copied {} {}".format(len(shorts), f))
+
+# CAREFUL: Pinpoint for united_kingdom was not found in data.
